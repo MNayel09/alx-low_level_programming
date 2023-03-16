8128bc463d4dff96397e524fcde34c898ca3cf5f
Mohamed Nayel <mohamed.abueleinein@gmail.com>
2023-03-16 07:45:40 -0700

    print using puts Programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-int puts(const char* Programming is like building a multilingual puzzle)
+#include<stdio.h>
+/**
+ * main - is a regular function
+ *
+ * Return: =0
+ */
+int main (void)
+{
+	char Mystr[] = "Programming is like building a multilingual puzzle";
+	puts(Mystr);
+	return (0);
+}
