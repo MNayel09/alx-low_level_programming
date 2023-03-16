b1f1d5385ce76cc7c2e64d5828337b42ca60fe0f
Mohamed Nayel <mohamed.abueleinein@gmail.com>
2023-03-16 07:59:51 -0700

    print using puts Programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	char Mystr[] = "Programming is like building a m	ultilingual puzzle";
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(Mystr);
 	return (0);
