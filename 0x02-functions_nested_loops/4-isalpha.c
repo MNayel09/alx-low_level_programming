dddfdc5f686f38af4346f069309080a1d8f161d7
Mohamed Nayel <mohamed.abueleinein@gmail.com>
2023-03-22 08:49:11 -0700

    update

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha - check if alpha
+ * @c: argument to check
+ *
+ * Return: =0
+ */
+int _isalpha(int c)
+{
+	if (_isalpha(c))
+		return (1);
+	else
+		return (0);
+	_putchar('\n');
+}
