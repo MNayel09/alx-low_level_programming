6b414fcd0210983eb079d25d7a0200495ad324b4
Mohamed Nayel <mohamed.abueleinein@gmail.com>
2023-03-22 15:01:38 -0700

    update

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_to_98 - print to 98
+ * @n: starting point
+ *
+ * Return: =0
+ */
+void print_to_98(int n)
+{
+	while (n <= 98)
+		_putchar(n);
+	n++;
+	_putchar('\n');
+}
