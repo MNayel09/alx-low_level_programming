26fe78ab3cb24c27affee8513182363e3257d129
Mohamed Nayel <mohamed.abueleinein@gmail.com>
2023-03-22 11:11:48 -0700

    update

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - absolute func
+ * @j: argument var
+ *
+ * Return: =0
+ */
+int _abs(int j)
+{
+	if (j <= 0 && j >= 0)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+	return (0);
+}
