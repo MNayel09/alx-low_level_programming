b936d0900178ec26d2d2324a68d121bbc4104305
Mohamed Nayel <mohamed.abueleinein@gmail.com>
2023-03-21 17:09:10 -0700

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-	char Ar[] = {"_putchar"};
+	char st[] = "_putchar";
 	int i;
 
-	for (i = 0; i > 8; i++)
+	for (i = 0; i < 8; i++)
 	{
-		putchar(Ar[i]);
+	putchar(st[i]);
 	}
 	putchar('\n');
 	return (0);
