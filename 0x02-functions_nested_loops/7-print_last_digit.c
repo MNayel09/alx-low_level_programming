2ad24e07989a364133ca2c380cbfe21bfd8870c3
Mohamed Nayel <mohamed.abueleinein@gmail.com>
2023-03-22 11:31:31 -0700

    update

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - prints last digit of a number
+ * @j: argument char
+ *
+ * Return: =0
+ */
+int print_last_digit(int j)
+{
+	if (j >= 0)
+		return (j);
+	return (0);
+}
