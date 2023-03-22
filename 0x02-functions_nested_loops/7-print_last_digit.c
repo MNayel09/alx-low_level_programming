2dad73e909f7e075447be47d89913ef09c29c614
Mohamed Nayel <mohamed.abueleinein@gmail.com>
2023-03-22 12:51:32 -0700

    update

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,13 +10,8 @@ int print_last_digit(int j)
 	int i = j % 10;
 
 	if (j >= 0)
-	{
-		for (i = 0; i < 2; i++)
-			{
-				return (i);
-			}
-	}
-		else if (j < 0)
+		return (i);
+	else if (j < 0)
 		return (i);
 	return (i);
 }
