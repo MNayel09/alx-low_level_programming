58ae7af4bc4d4310e171d4f05fe569907bf751db
Mohamed Nayel <mohamed.abueleinein@gmail.com>
2023-03-23 19:04:35 -0700

    update

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+/**
+ * main - FizzBuzz game
+ *
+ * Return: =0
+ */
+int main(void)
+{
+	int a;
+
+	a = 1;
+	while (a < 101)
+	{
+		if (a % 3 == 0)
+		{
+			printf("Fizz  ");
+		}
+		else if (a % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (a % 3 == 0 && a % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", a);
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
