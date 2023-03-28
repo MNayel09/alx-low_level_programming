0d7ea4dd9e5fe974efa06503da08494c03f69ab1
Mohamed Nayel <mohamed.abueleinein@gmail.com>
2023-03-27 18:40:40 -0700

    update

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include<string.h>
+#include "main.h"
+/**
+ * print_rev - print string in reverse
+ * @s: string arg
+ *
+ * Return: =0
+ */
+void print_rev(char *s)
+{
+	int count = strlen(s);
+	int i;
+
+	for (i = count; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
