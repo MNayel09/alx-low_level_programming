12ab95e2230da12cc4275091a7cf8ecc04f3957f
Mohamed Nayel <mohamed.abueleinein@gmail.com>
2023-03-31 13:05:13 -0700

    update

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * reverse_array - reverse the content of an array
+ * @a: arg1
+ * @n: arg2
+ *
+ * Return: =0
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+
+	for (i = n; i >= 0; i--)
+		a[i];
+}
