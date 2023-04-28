4a1b4d10728295f7da554ed517f5fec2828bef45
Mohamed Nayel <mohamed.abueleinein@gmail.com>
2023-04-28 03:00:46 -0700

    Hello holberton by NASM

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,24 +1,19 @@
 section .data
-	message db 'Hello, Holberton',0
-	format db '%s', 10, 0
+	message: db "Hello, Holberton",10,0
+	format: db "%s",0
 
 section .text
 	global main
+	extern printf
 
 main:
-	; Set up stack frame
 	push rbp
 	mov rbp, rsp
 
-	; Pass arguments to printf and call it
 	mov rdi, format
 	mov rsi, message
-	xor rax, rax
 	call printf
 
-	; Clean up stack and exit
-	mov rsp, rbp
 	pop rbp
-	xor eax, eax
+	mov rax, 0
 	ret
-
