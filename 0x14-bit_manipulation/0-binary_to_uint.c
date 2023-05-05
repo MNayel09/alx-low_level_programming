ab1c9dd4f0dc9c5abe444a1c1bf60613b1a7b69f
Mohamed Nayel <mohamed.abueleinein@gmail.com>
2023-05-05 12:11:05 -0700

    Binary to U-int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -16,9 +16,15 @@ unsigned int binary_to_uint(const char *b)
 	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] == '0')
-			result = (result << 1) | 0;
+		{
+			result = result << 1;
+				return (0);
+		}
 		else if (b[i] == '1')
-			result = (result << 1) | 1;
+		{
+			result = result << 1;
+				return (1);
+		}
 		else
 			return (0);
 	}
