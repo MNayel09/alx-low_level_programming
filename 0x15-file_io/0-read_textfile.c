725d7500e68382c19d7a4a253da1af36c57b56d4
Mohamed Nayel <mohamed.abueleinein@gmail.com>
2023-05-08 14:19:52 -0700

    Read textfile and write to a new file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * read_textfile - Reads and prints the content of a text file to standard output.
- * @filename: The name of the file to read.
+ * read_textfile - Reads a text file and prints it to the POSIX standard output.
+ * @filename: The name of the file to be read.
  * @letters: The number of letters to read and print.
  *
  * Return: The actual number of letters read and printed, or 0 on failure.
@@ -10,29 +10,34 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t bytes_read, bytes_written;
-	char buffer[1024];
+	char *buffer;
 
 	if (filename == NULL)
 		return 0;
-
+	buffer = malloc(sizeof(char) * (letters + 1));
+	if (buffer == NULL)
+		return 0;
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
+	{
+		free(buffer);
 		return 0;
-
-	bytes_read = read(fd, buffer, sizeof(buffer));
+	}
+	bytes_read = read(fd, buffer, letters);
 	if (bytes_read == -1)
 	{
+		free(buffer);
 		close(fd);
 		return 0;
 	}
-	if (letters < (size_t)bytes_read)
-		bytes_read = letters;
 	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 	if (bytes_written == -1 || (size_t)bytes_written != (size_t)bytes_read)
 	{
+		free(buffer);
 		close(fd);
 		return 0;
 	}
+	free(buffer);
 	close(fd);
 	return bytes_written;
 }
