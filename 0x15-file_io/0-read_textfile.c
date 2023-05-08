2d245f8866d9e7de0e53ddf85a8d4604812ffc23
Mohamed Nayel <mohamed.abueleinein@gmail.com>
2023-05-08 14:00:34 -0700

    Read textfile and write toa new file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * read_textfile - Reads and prints the content of a text file to standard output.
+ * @filename: The name of the file to read.
+ * @letters: The number of letters to read and print.
+ *
+ * Return: The actual number of letters read and printed, or 0 on failure.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t bytes_read, bytes_written;
+	char buffer[1024];
+
+	if (filename == NULL)
+		return 0;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return 0;
+
+	bytes_read = read(fd, buffer, sizeof(buffer));
+	if (bytes_read == -1)
+	{
+		close(fd);
+		return 0;
+	}
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written == -1 || (size_t)bytes_written != (size_t)bytes_read)
+	{
+		close(fd);
+		return 0;
+	}
+	close(fd);
+	return bytes_written;
+}
