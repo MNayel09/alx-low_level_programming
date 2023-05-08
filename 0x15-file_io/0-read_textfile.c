9c6e56437c19199255c3e300672dcd4500457230
Mohamed Nayel <mohamed.abueleinein@gmail.com>
2023-05-08 14:33:32 -0700

    Read textfile and write to a new file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standard output.
+ * read_textfile - Reads textfile and prints it to the POSIX standard output.
  * @filename: The name of the file to be read.
  * @letters: The number of letters to read and print.
  *
@@ -13,31 +13,31 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buffer;
 
 	if (filename == NULL)
-		return 0;
-	buffer = malloc(sizeof(char) * (letters));
+		return (0);
+	buffer = malloc(sizeof(char) * (letters + 1));
 	if (buffer == NULL)
-		return 0;
+		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
 		free(buffer);
-		return 0;
+		return (0);
 	}
 	bytes_read = read(fd, buffer, letters);
 	if (bytes_read == -1)
 	{
 		free(buffer);
 		close(fd);
-		return 0;
+		return (0);
 	}
 	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	if (bytes_written == -1)
+	if (bytes_written == -1 || (size_t)bytes_written != (size_t)bytes_read)
 	{
 		free(buffer);
 		close(fd);
-		return 0;
+		return (0);
 	}
 	free(buffer);
 	close(fd);
-	return bytes_written;
+	return (bytes_written);
 }
